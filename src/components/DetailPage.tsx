7c4c3f6f3ef2ad57be7b5a931230048520254e14
leeal <leealexandra58@gmail.com>
2022-12-12 11:46:25 -0500

    initial commit, unfinished

--- src/components/DetailPage.tsx
+++ src/components/DetailPage.tsx
@@ -4,6 +4,7 @@ import { BusinessInfo } from "../models/Details";
 import { Review, Reviews } from "../models/Reviews";
 import { getDetails, getReviews } from "../services/YelpApi";
 import "./DetailPage.css"
+import { GiRoundStar } from 'react-icons/gi'
 
 export const DetailPage = () => {
 const [business, setBusiness] = useState<BusinessInfo>();
@@ -28,20 +29,20 @@ useEffect(() => {
 
 return (
     <div className="detail-card">
-        <h2>{business?.name}</h2>
-        <div className="business-info">
-        <div>{business?.photos && business.photos.slice(0, 3).map((photo) => (
-            <img src={photo} alt={business.name}></img>
+        <div className="photos">{business?.photos && business.photos.slice(0, 3).map((photo) => (
+            <img className="detail-photo"src={photo} alt={business.name}></img>
         ))}</div>
+        <h2 className="detail-name">{business?.name}</h2>
+        <div className="business-info">
         <div className="text-info">
             <h4>Categories: {business?.categories && business.categories.slice(0, 3).map((category) => (
             <div>{category.title}</div>
             ))}</h4>
-            <h4>Address: {business?.location.display_address}</h4>
-            <h4>Rating: {business?.rating}</h4>
+            <h4><GiRoundStar color="#ffae42" />     {business?.rating}   from   {reviews?.total}   reviews</h4>
             <h4>Price: {business?.price}</h4>
+            <h4>Address: {business?.location.display_address}</h4>
             <h4>Phone: {business?.display_phone}</h4>
-            <h4>Hours: {business?.hours && business.hours.map((hour) => (
+            {/* <h4>Hours: {business?.hours && business.hours.map((hour) => (
                 <div>{hour.open && hour.open.slice(0, 7).map((open) => (
                     <div>
                         <p>{open.day}</p>
@@ -49,18 +50,25 @@ return (
                         <p>{open.end}</p>
                     </div>
                 ))}</div> 
-            ))}</h4>
-            <h4><a href={business?.url}>Website</a></h4>
+            ))}</h4> */}
+            <h4><a href={business?.url} target="_blank">Website</a></h4>
         </div>
         </div>
-        <h3>Reviews</h3>
+        <div className="review-container">
+        <h2 className="review-title">Reviews</h2>
         <h4>{reviews?.reviews && reviews.reviews.slice(0, 3).map((review) => (
-            <div>
-                <p>{review.user.name}</p>
-                <p>{review.rating}</p>
-                <p className="review-text">{review.text}</p>
+            <div className="reviews">
+                <div className="star-review">
+                    <p className="star"><GiRoundStar color="#ffae42" /></p>
+                    <p className="review-rating">{review.rating}</p>
+                </div>
+                <div>
+                    <p className="username">{review.user.name}</p>
+                    <p className="review-text">{review.text}<a href={business?.url} target="_blank">read more</a></p>
+                </div>
             </div>
         ))}</h4>
+        </div>
     </div>
 )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,20 @@
 import React, { useContext } from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { BsSearch } from 'react-icons/bs'
+import { IoSearchOutline } from 'react-icons/io5'
 import { FcGoogle } from 'react-icons/fc'
-import { IoLogOutOutline } from 'react-icons/io5'
+import { FiLogOut } from 'react-icons/fi'
 import { AiOutlineUnorderedList } from 'react-icons/ai'
 import "./Header.css"
 import { signInWithGoogle, signOut } from "../firebaseConfig";
 import AuthContext from '../context/AuthContext';
+import logoImg from '../logo-png.png'
 
 const Header = () => {
 
   const [hoverSearch, setHoverSearch] = useState(false);
   const [hoverList, setHoverList] = useState(false);
+  const [hoverSignOut, setHoverSignOut] = useState(false);
 
   const handleMouseOverSearch = () => {
     setHoverSearch(true);
@@ -30,32 +32,45 @@ const Header = () => {
     setHoverList(false);
   }
 
+  const handleMouseOverSignOut = () => {
+    setHoverSignOut(true);
+  };
+
+  const handleMouseOutSignOut = () => {
+    setHoverSignOut(false);
+  }
+
   const { user } = useContext(AuthContext);
 
   return (
     <div className="Header">
-        <div className="title">
-          <h1>BUDGET BON VOYAGE</h1>
-        </div>
+      <div className="logo-container">
+        <img className="logo" src={logoImg} alt="logo"></img>
+      </div>
 
         <div className="nav-btns">
 
-        <div className="sign-in-nav-btn">
-            { user ? <h3>Welcome {user?.displayName} 
-                <button className="sign-out-button" onClick={signOut}>Sign Out<IoLogOutOutline size={22} /></button></h3> :
-                <button className="sign-in-button" onClick={signInWithGoogle}>Sign In With Google<FcGoogle size={20} /></button>
+        <div className="sign-in-container">
+            { user ? <div className="welcome-container"><p className="welcome-text">Welcome {user?.displayName}</p>
+                <div className="bleh">
+                <button className="sign-out-button" onClick={signOut} onMouseOver={handleMouseOverSignOut} onMouseOut={handleMouseOutSignOut}><FiLogOut color="#ffae42" size={20} /></button>{hoverSignOut && (
+                  <div className="sign-out-text">
+                      Sign Out
+                  </div>
+              )}</div></div> :
+                <div className="sign-in"><button className="sign-in-button" onClick={signInWithGoogle}><FcGoogle size={20} /></button><p className="sign-in-text">Sign In With Google</p></div>
             }
         </div>
         
         <div className="search-nav-btn">
             <Link to="/">
                 <button className="search-button" onMouseOver={handleMouseOverSearch} onMouseOut={handleMouseOutSearch}>
-                    <BsSearch size={18}/>
+                    <IoSearchOutline color="#ffae42" size={20}/>
                 </button>
             </Link>
             {hoverSearch && (
                 <div className="search-text">
-                    Search
+                  Search
                 </div>
             )}
         </div>
@@ -63,7 +78,7 @@ const Header = () => {
         <div className="list-nav-btn">
             <Link to="/addlist">
                 <button className="list-button" onMouseOver={handleMouseOverList} onMouseOut={handleMouseOutList}>
-                    <AiOutlineUnorderedList size={20}/>
+                    <AiOutlineUnorderedList color="#ffae42" size={20}/>
                 </button>
             </Link>
             {hoverList && (
